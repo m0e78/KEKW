b6eb294ae22c6d475c5e60bb2931d3a4244f2669
mo <moezzanza@gmail.com>
2024-02-12 20:24:42 +0100

    header done...

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../components/navbar/Navbar"
-import Header from "../../components/navbar/header/Header"
+import Header from "../../components/header/Header"
 import "./home.css"
 
 const Home = () => {
