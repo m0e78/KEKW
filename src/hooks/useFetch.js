409236ec1aa48e63dd48491a354dc468e09a8bca
moe <moezzanza@gmail.com>
2024-03-07 18:41:41 +0100

    ...

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useEffect, useState } from "react"
 const useFetch = (url) => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
